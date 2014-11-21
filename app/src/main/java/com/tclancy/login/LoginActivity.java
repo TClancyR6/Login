adad605ffa028ec6d6430a346bd94390bf0a4bd3
khkim <tclancyr6@gmail.com>
2014-11-21 11:10:39 +0900

    test

--- app/src/main/java/com/tclancy/login/LoginActivity.java
+++ app/src/main/java/com/tclancy/login/LoginActivity.java
@@ -14,6 +14,7 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // commit test
     }
 
     @Override
